42f14c8f4780b741e9c3d81c2746f2233b6fc330
Igor Silveira <igor-silveira10@hotmail.com>
2026-02-12 21:09:34 -0300

    Implement Block Kit approval buttons for Slack adapter

--- pkg/channels/slack/slack.go
+++ pkg/channels/slack/slack.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log/slog"
 	"os"
+	"strings"
 	"sync"
 
 	"github.com/igorsilveira/pincer/pkg/channels"
@@ -105,6 +106,45 @@ func (a *Adapter) Capabilities() channels.ChannelCaps {
 	}
 }
 
+func (a *Adapter) SendApprovalRequest(ctx context.Context, req channels.ApprovalRequest) error {
+	if a.client == nil {
+		return fmt.Errorf("slack: not connected")
+	}
+
+	a.mu.RLock()
+	var channelID string
+	for cid, sid := range a.sessions {
+		if sid == req.SessionID {
+			channelID = cid
+			break
+		}
+	}
+	a.mu.RUnlock()
+
+	if channelID == "" {
+		return fmt.Errorf("slack: no channel for session %s", req.SessionID)
+	}
+
+	headerText := slackapi.NewTextBlockObject(slackapi.MarkdownType,
+		fmt.Sprintf("*Tool approval needed: %s*\nInput: `%s`", req.ToolName, req.Input), false, false)
+	headerSection := slackapi.NewSectionBlock(headerText, nil, nil)
+
+	approveBtn := slackapi.NewButtonBlockElement("approval:approve:"+req.RequestID, "approve",
+		slackapi.NewTextBlockObject(slackapi.PlainTextType, "Approve", false, false))
+	approveBtn.Style = slackapi.StylePrimary
+
+	denyBtn := slackapi.NewButtonBlockElement("approval:deny:"+req.RequestID, "deny",
+		slackapi.NewTextBlockObject(slackapi.PlainTextType, "Deny", false, false))
+	denyBtn.Style = slackapi.StyleDanger
+
+	actions := slackapi.NewActionBlock("approval_actions", approveBtn, denyBtn)
+
+	_, _, err := a.client.PostMessageContext(ctx, channelID,
+		slackapi.MsgOptionBlocks(headerSection, actions),
+	)
+	return err
+}
+
 func (a *Adapter) listenEvents(ctx context.Context) {
 	for {
 		select {
@@ -130,6 +170,43 @@ func (a *Adapter) handleEvent(evt socketmode.Event) {
 		}
 		a.socket.Ack(*evt.Request)
 		a.handleEventsAPI(data)
+
+	case socketmode.EventTypeInteractive:
+		callback, ok := evt.Data.(slackapi.InteractionCallback)
+		if !ok {
+			return
+		}
+		a.socket.Ack(*evt.Request)
+		a.handleInteraction(callback)
+	}
+}
+
+func (a *Adapter) handleInteraction(callback slackapi.InteractionCallback) {
+	if len(callback.ActionCallback.BlockActions) == 0 {
+		return
+	}
+
+	action := callback.ActionCallback.BlockActions[0]
+	parts := strings.SplitN(action.ActionID, ":", 3)
+	if len(parts) != 3 || parts[0] != "approval" {
+		return
+	}
+
+	act := parts[1]
+	requestID := parts[2]
+	approved := act == "approve"
+
+	channelID := callback.Channel.ID
+	sessionID := a.sessionForChannel(channelID)
+
+	a.inbound <- channels.InboundMessage{
+		ChannelName: "slack",
+		SessionID:   sessionID,
+		PeerID:      callback.User.ID,
+		ApprovalResponse: &channels.InboundApprovalResponse{
+			RequestID: requestID,
+			Approved:  approved,
+		},
 	}
 }
 
@@ -179,4 +256,10 @@ func (a *Adapter) getOrCreateSession(channelID string) string {
 	sid = fmt.Sprintf("sl-%s", channelID)
 	a.sessions[channelID] = sid
 	return sid
+}
+
+func (a *Adapter) sessionForChannel(channelID string) string {
+	a.mu.RLock()
+	defer a.mu.RUnlock()
+	return a.sessions[channelID]
 }
